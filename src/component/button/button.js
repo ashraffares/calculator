8db14855a7cb41ce645137dbeee9f019b1df3fe9
ashraffares <ashraffares090@gmail.com>
2021-08-11 18:09:23 +0200

    Add button and display component

---
+++ src/component/button/button.js
@@ -0,0 +1,9 @@
+import propTypes from 'prop-types';
+
+const Button = ({ name }) => <button type="button">{name}</button>;
+
+Button.propTypes = {
+  name: propTypes.string.isRequired,
+};
+
+export default Button;
