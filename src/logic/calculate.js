a664b53ba63aef817203ad5a8085733668f24fba
ashraffares <ashraffares090@gmail.com>
2021-08-17 13:31:48 +0200

    use operate to preform basic calculation

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,7 +1,20 @@
 import operate from './operate';
 
-const calculate = (obj , btnName) => {
+const calculate = (obj, btnName) => {
+  if (btnName === '+/-') {
+    (obj.total * (-1)).toString();
+    (obj.next * (-1)).toString();
+    return obj;
+  }
 
-}
+  if (obj.operation) {
+    return {
+      total: operate(obj.total, obj.next, obj.operation),
+      next: null,
+      operation: btnName,
+    };
+  }
+  return obj;
+};
 
 export default calculate;
