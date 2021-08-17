89581b58711af73eeec0804ba95def722fc0db45
ashraffares <ashraffares090@gmail.com>
2021-08-17 16:42:06 +0200

    Add all the cases

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -4,16 +4,8 @@ function isNumber(item) {
   return !!item.match(/[0-9]+/);
 }
 
-/**
- * Given a button name and a calculator data object, return an updated
- * calculator data object.
- *
- * Calculator data object contains:
- *   total:s      the running total
- *   next:String       the next number to be operated on with the total
- *   operation:String  +, -, etc.
- */
 export default function calculate(obj, buttonName) {
+  // if buttonName is AC return empty obj
   if (buttonName === 'AC') {
     return {
       total: '',
@@ -26,14 +18,14 @@ export default function calculate(obj, buttonName) {
     if (buttonName === '0' && obj.next === '0') {
       return {};
     }
-    // If there is an operation, update next
+
     if (obj.operation) {
       if (obj.next) {
         return { next: obj.next + buttonName };
       }
       return { next: buttonName };
     }
-    // If there is no operation, update next and clear the value
+
     if (obj.next) {
       return {
         next: obj.next + buttonName,
@@ -74,7 +66,6 @@ export default function calculate(obj, buttonName) {
       };
     }
 
-    // '=' with no operation, nothing to do
     return {};
   }
 
@@ -88,15 +79,6 @@ export default function calculate(obj, buttonName) {
     return {};
   }
 
-  // Button must be an operation
-
-  // When the user presses an operation button without having entered
-  // a number first, do nothing.
-  // if (!obj.next && !obj.total) {
-  //   return {};
-  // }
-
-  // User pressed an operation button and there is an existing operation
   if (obj.operation) {
     if (obj.total && !(obj.next)) {
       return {
@@ -110,14 +92,10 @@ export default function calculate(obj, buttonName) {
     };
   }
 
-  // no operation yet, but the user typed one
-
-  // The user hasn't typed a number yet, just save the operation
   if (!obj.next) {
     return { operation: buttonName };
   }
 
-  // save the operation and shift 'next' into 'total'
   return {
     total: obj.next,
     next: null,
