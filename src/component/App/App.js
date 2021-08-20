9a719cc16f96a13191d9a26c5002cbaf4d8a61ca
ashraffares <ashraffares090@gmail.com>
2021-08-20 14:30:53 +0200

    Add state to app with three propties total-next-operation

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -5,13 +5,25 @@ import Display from '../Display';
 // eslint-disable-next-line no-unused-vars
 import calculate from '../../logic/calculate';
 
-function App() {
-  return (
-    <React.Fragment className="App">
-      <Display />
-      <ButtonPanel />
-    </React.Fragment>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <Display />
+        <ButtonPanel />
+      </>
+    );
+  }
 }
 
 export default App;
