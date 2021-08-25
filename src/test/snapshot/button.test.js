06f4e9a9e41708ec8a45016d4b885a6fb1f43bc5
ashraffares <ashraffares090@gmail.com>
2021-08-25 22:25:12 +0200

    Add test for calculate operation

--- src/test/snapshot/button.test.js
+++ src/test/snapshot/button.test.js
@@ -4,7 +4,7 @@ import Button from '../../component/button';
 describe('Button component', () => {
   it('matches the snapshot', () => {
     const handleClick = () => 'just for testing';
-    const button = renderer.create(<Button clickHandler={handleClick} name="1" />).toJSON();
+    const button = renderer.create(<Button handleClick={handleClick} name="1" />).toJSON();
     expect(button).toMatchSnapshot();
   });
 });
